a229145d4a912d269b847645cf8b7be4973bc2b8
amh1k <abdulmoizx97@gmail.com>
2026-03-03 15:39:46 +0500

    add queue logic to monitor service unit test

--- tests/unit/services/monitor.service.test.ts
+++ tests/unit/services/monitor.service.test.ts
@@ -1,6 +1,12 @@
 import { describe, it, expect, vi } from "vitest";
 import { MonitorService } from "../../../src/services/monitor.service";
 import { prismaMock } from "../../singleton";
+import { monitorQueue } from "../../../src/queues/monitor.queue";
+vi.mock("../../../src/queues/monitor.queue", () => ({
+  monitorQueue: {
+    add: vi.fn(),
+  },
+}));
 describe("MonitorService unit tests", () => {
   it("Should create a monitor and return saved object", async () => {
     const monitorInput = {
@@ -29,9 +35,18 @@ describe("MonitorService unit tests", () => {
     expect(prismaMock.monitor.create).toHaveBeenCalledWith({
       data: expect.objectContaining({ name: "Google" }),
     });
+    expect(monitorQueue.add).toHaveBeenCalledWith(
+      `ping-mon-999`,
+      { monitorId: "mon-999" },
+      expect.objectContaining({
+        repeat: {
+          every: 60000,
+        },
+      }),
+    );
   });
 
-  it("should throw an error if database creation fails", async () => {
+  it("should throw an error if database creation fails or adding to queue fails", async () => {
     const errorMessage = "Database connection failed";
     prismaMock.monitor.create.mockRejectedValue(new Error(errorMessage));
     await expect(MonitorService.create({} as any)).rejects.toThrow(
